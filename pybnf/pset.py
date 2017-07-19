a04cc21d3d9758c91d94b5edb33b4ca1430736c3
Eshan Mitra <emitra@lanl.gov>
2017-07-19 14:43:51 -0600

    First attempt at the Pset class, untested.

--- pybnf/pset.py
+++ pybnf/pset.py
@@ -3,26 +3,54 @@ import numpy as np
 import re
 
 
-class Pset(object):
-    """Class representing a parameter set"""
+class Pset(dict):
+    """
+    Class representing a parameter set
 
-    def __init__(self):
-        pass
-        # Initialize parameters (self.x = ...)
+    Pset is a subclass of the built-in dict class, intended to contain str keys and float values, of the form
+    {'paramname1':paramvalue1}
 
-    @staticmethod
-    def to_number(x):
+    Pset can be constructed the same way as a dictionary, either by passing a dict to the constructor, or by passing no
+    arguments to the constructor, and subsequently setting individual parameters
+
+    The standard dict syntax may be used to access individual parameter values, e.g. ps['paramname1']
+
+    """
+
+    def __init__(self,iterable,**kwargs):
+
+        dict.__init__(self, iterable, **kwargs)
+
+
+    def get_id(self):
         """
-        Attempts to convert a string to a float
+        Returns a unique identifier for this parameter set
+        Two Psets will have the same identifier if they have the same keys and corresponding values
 
-        :param x: str
-        :return: float
+        :return: int
         """
-        if re.match('\b[nN][aA][nN]\b', x):
-            return math.nan
-        elif re.match('\b[iI][nN][fF]\b', x):
-            return math.inf
-        elif re.match('\b-[iI][nN][fF]\b', x):
-            return -math.inf
-        else:
-            return float(x)
+        unique_str = ''.join([self.keys_to_string(),self.values_to_string()])
+        return hash(unique_str)
+
+    def keys_to_string(self):
+        """
+        Returns the keys (parameter names) in a tab-separated str in alphabetical order
+
+        :return: str
+        """
+        keys = [str(k) for k in self.keys()]
+        keys.sort()
+        return '\t'.join(keys)
+
+    def values_to_string(self):
+        """
+        Returns the parameter values in a tab-separated str, in alphabetical order
+        according to the parameter name
+        :return: str
+        """
+        keys = [str(k) for k in self.keys()]
+        keys.sort()
+        values = [str(self[k]) for k in keys()] #Values are in alpha order by key name
+        return'\t'.join(values)
+
+
--- tests/test_pset_class.py
+++ tests/test_pset_class.py
@@ -1,5 +1,5 @@
 import math
-from .context import data
+from .context import pset
 from nose.tools import raises
 
 
@@ -10,12 +10,47 @@ class TestPset:
     @classmethod
     def setup_class(cls):
         """Define constants to be used in tests"""
-        cls.str0 = "inf"
-        #etc..
+        cls.dict2 = {'x':1.0,'y':2.0,'z':3.14}
+        cls.dict3 = {'xx':3.0, 'yy':700.3, 'ww':-5.2e6,'kk':5.2e-39}
+        cls.dict4 = {'kk': 5.2e-39, 'xx': 3.00, 'yy': 700.3, 'ww': -5.2e6}
+        cls.dict5 = {'x':1.0,'y':2.0,'z':3.141}
 
-    def test_number_reader(self):
-        pass
-        # assert function_call() == expected answer
+    def test_initialization(self):
+        ps1 = pset.Pset(self.dict2)
+        assert ps1['x']==1.0
+        assert ps1['z']==3.14
+
+        ps2 = pset.Pset()
+        ps2['p1'] = 2.5
+        ps2['p2'] = 4.5
+        assert ps2['p1'] == 2.5
+        assert ps2['p2'] == 4.5
+
+    def test_keys_to_string(self):
+        ps1 = pset.Pset(self.dict2)
+        assert ps1.keys_to_string() == 'x\ty\tz'
+        ps2 = pset.Pset(self.dict3)
+        assert ps2.keys_to_string() == 'kk\tww\txx\tyy'
+
+    def test_values_to_string(self):
+        ps1 = pset.Pset(self.dict2)
+        assert ps1.values_to_string == '1.0\t2.0\t3.14'
+        ps2 = pset.Pset(self.dict3)
+        assert ps2.values_to_string() == '5.2e-39\t-0.000052\t3.0\t700.3'
+
+    def test_get_id(self):
+        ps1a = pset.Pset(self.dict2)
+        ps1b = pset.Pset(self.dict2)
+        ps2a = pset.Pset(self.dict3)
+        ps2b = pset.Pset(self.dict4)
+        ps3 = pset.Pset(self.dict5)
+        assert ps1a.get_id() == ps1b.get_id()
+        assert ps2a.get_id() == ps2b.get_id()
+        assert ps1a.get_id() != ps2a.get_id()
+        assert ps3.get_id() != ps1a.get_id()
+
+
+
 
 
     #@raises(ValueError)
