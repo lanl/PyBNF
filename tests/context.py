07bef1c2f178bc294010ff8c90736431bc5322c6
Ryan Suderman <ryants@lanl.gov>
2017-07-20 10:51:18 -0600

    added context file to facilitate importing modules to test files

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Add imports here for importing into test files
+# For example:
+#     import pybnf.data as data
+# will allow test files to import the data module:
+#     from .context import data
