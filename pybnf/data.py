209d09c323b824e5851e409d3a829c2638e0c7de
Ryan Suderman <ryants@lanl.gov>
2017-07-21 10:47:39 -0600

    implemented __getitem__ and function for data loading from file

--- pybnf/data.py
+++ pybnf/data.py
@@ -8,7 +8,18 @@ class Data(object):
 
     def __init__(self):
         self.cols = dict()  # dictionary with column header, column index pairs
-        self.data = np.array()  # Numpy array for data
+        self.data = None  # Numpy array for data
+
+    def __getitem__(self, col_header):
+        """
+        Gets a column of data based on its column header
+        
+        :param col_header: Data column name
+        :type col_header: str
+        :return: Numpy array corresponding to name
+        """
+        idx = self.cols[col_header]
+        return self.data[:, idx]
 
     @staticmethod
     def to_number(x):
@@ -27,3 +38,31 @@ class Data(object):
         else:
             return float(x)
 
+    def load_data(self, file_name, sep='\s+'):
+        """
+        Loads column data from a text file
+
+        :param file_name: Name of data file
+        :type file_name: str
+        :param sep: String that separates columns
+        :type sep: str
+        :return: None
+        """
+        with open(file_name) as f:
+            lines = f.readlines()
+
+        self.data = self._read_file_lines(lines, sep)
+
+    def _read_file_lines(self, lines, sep):
+        """Helper function that reads lines from BNGL gdat files"""
+
+        header = re.split(sep, lines[0].strip().strip('#').strip())
+
+        for c in header:
+            self.cols[c] = len(self.cols)
+
+        data = []
+        for l in lines[1:]:
+            data.append([self.to_number(x) for x in re.split(sep, l.strip())])
+
+        return np.array(data)
--- tests/test_data_class.py
+++ tests/test_data_class.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from .context import data
 from nose.tools import raises
 
@@ -15,6 +16,14 @@ class TestData:
         cls.str3 = "6.022e-23"
         cls.str4 = "abc"
 
+        cls.data0 = [
+            '#          time    fullyBoundAg        Bound2Ag        Bound1Ag          freeAg    fullyBoundAb\n',
+            ' 0.00000000e+00  1.20000000e+01  8.00000000e+00  6.00000000e+00  0.00000000e+00  1.60000000e+01\n',
+            ' 1.00000000e+00  1.20000000e+01  8.00000000e+00  6.00000000e+00  0.00000000e+00  1.60000000e+01\n'
+        ]
+        cls.d = data.Data()
+        cls.d.data = cls.d._read_file_lines(cls.data0, '\s+')
+
     def test_number_reader(self):
         assert data.Data.to_number(self.str0) == math.inf
         assert not math.isfinite(data.Data.to_number(self.str1))
@@ -24,3 +33,19 @@ class TestData:
     @raises(ValueError)
     def test_number_reader_failure(self):
         data.Data.to_number(self.str4)
+
+    def test_read_file_lines(self):
+        d = data.Data()
+        loc_data = d._read_file_lines(self.data0, '\s+')
+        d.data = loc_data
+        assert d.cols['time'] == 0
+        assert len(d.cols.keys()) == 6
+        assert d.data.shape == (2, 6)
+        assert d.data[0, 2] == 8
+
+    def test_column_access(self):
+        assert np.array_equal(self.d['time'], np.array([0, 1]))
+
+    @raises(KeyError)
+    def test_column_access_failure(self):
+        self.d['thing']
