2d780af8e755122e66c0bc13c377b635cc2bfa6e
Ryan Suderman <ryants@lanl.gov>
2017-09-07 11:30:48 -0600

    added structure for entry point

---
+++ pybnf/__main__.py
@@ -0,0 +1,5 @@
+"""pybnf.__main__: executed when directory is called as a script"""
+
+from .pybnf import main
+
+main()
---
+++ pybnf/pybnf.py
@@ -0,0 +1,8 @@
+"""pybnf.pybnf: defines the entry point for the PyBNF application"""
+
+
+__version__ = "0.1"
+
+
+def main():
+    pass
