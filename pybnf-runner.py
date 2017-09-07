0236dd99e667f3820ee28f36a9ba6b92dd387272
Ryan Suderman <ryants@lanl.gov>
2017-09-07 11:33:36 -0600

    convenience script for running PyBNF

---
+++ pybnf-runner.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+
+"""truml.truml-runner: Convenience script for running PyBNF from the source tree"""
+
+
+from pybnf.pybnf import main
+
+if __name__ == '__main__':
+    main()
