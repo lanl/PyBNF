c5906f992026ac7be21c1d0db9f951f70218ed6f
ionif <aionkov@gmail.com>
2017-09-18 15:15:01 -0600

    init arg

--- pybnf/pybnf.py
+++ pybnf/pybnf.py
@@ -3,6 +3,14 @@
 
 __version__ = "0.1"
 
+import argparse
 
 def main():
     print("PyBNF-- version %s" % __version__)
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-c', action='store_const', dest='conf_file',
+                        const='value-to-store', help='config file path')
+    
+    results = parser.parse_args()
+    print(results.conf_file)
