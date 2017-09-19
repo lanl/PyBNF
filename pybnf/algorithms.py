4313390c193e75df1bfd71f77c49f0d907ecfd63
Ryan Suderman <ryants@lanl.gov>
2017-09-19 15:15:16 -0600

    init function for Algorithm class

--- pybnf/algorithms.py
+++ pybnf/algorithms.py
@@ -1,6 +1,9 @@
 """pybnf.algorithms: contains the Algorithm class and subclasses as well as support classes and functions"""
 
 
+from .pset import Trajectory
+
+
 class Result(object):
     def __init__(self, paramset, simdata):
         self.pset = paramset
@@ -14,3 +17,20 @@ class Job:
 
     def run_simulation(self):
         pass
+
+
+class Algorithm(object):
+    def __init__(self, exp_data, objective):
+        """
+        Instantiates an Algorithm with a set of experimental data and an objective function.  Also
+        initializes a Trajectory instance to track the fitting progress
+
+        :param exp_data: List of experimental Data objects to be fit
+        :type exp_data: iterable
+        :param objective: The objective function
+        :type objective: ObjectiveFunction
+        """
+        self.exp_data = exp_data
+        self.objective = objective
+        self.trajectory = Trajectory()
+
