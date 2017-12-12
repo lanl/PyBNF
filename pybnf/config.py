e5fed193150fb08e91d4c74425e506f32fe2d371
Ryan Suderman <ryants@lanl.gov>
2017-12-12 10:52:08 -0700

    added wall_time parameter

--- pybnf/config.py
+++ pybnf/config.py
@@ -70,7 +70,12 @@ class Configuration(object):
             'credible_intervals': [68., 95.],
 
             'simplex_step': 1.0, 'simplex_reflection': 1.0, 'simplex_expansion':1.0, 'simplex_contraction': 0.5,
-            'simplex_shrink': 0.5
+            'simplex_shrink': 0.5,
+
+            'bng_command': bng_command,
+            'output_dir': 'bnf_out',
+
+            'wall_time': 3600
         }
         return default
 
