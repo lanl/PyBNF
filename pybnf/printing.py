f57ec74a72e4534ba3bb309aa035be999ac01660
Eshan Mitra <emitra@lanl.gov>
2018-01-26 16:45:01 -0700

    Added documentation for the new feature.

--- pybnf/printing.py
+++ pybnf/printing.py
@@ -6,14 +6,17 @@ verbosity = 1
 
 
 def print0(s):
+    """Print the statement at any verbosity level"""
     print(s)
 
 
 def print1(s):
+    """Print the statement only if the verbosity level is at least 1"""
     if verbosity >= 1:
         print(s)
 
 
 def print2(s):
+    """Print the statement only if the verbosity level is 2"""
     if verbosity >= 2:
         print(s)
