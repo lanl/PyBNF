e48450c1080ae3f9a9b49c3b4a1474191e5eb642
Eshan Mitra <emitra@lanl.gov>
2018-01-29 11:21:30 -0700

    Pybnf error takes 2 messages: one for the log and one for the user, or a single message for both.

--- pybnf/printing.py
+++ pybnf/printing.py
@@ -27,5 +27,14 @@ class PybnfError(RuntimeError):
     Represents a user-generated error for which we can provide an informative message to the user about what
     went wrong with the input before quitting.
     """
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, log_message, user_message=None):
+        """
+        :param log_message: The message to print to the log
+        :param user_message: The message to output to the user. If omitted, the user gets the same message as
+        log_message.
+        """
+        self.log_message = log_message
+        if user_message:
+            self.message = user_message
+        else:
+            self.message = log_message
