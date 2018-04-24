955237dc0a600c1f1450a7f351c57dcef5d98a72
Eshan Mitra <emitra@lanl.gov>
2018-04-24 11:59:40 -0600

    Added batch script for running example3 on SLURM cluster

---
+++ examples/example3/ex3_batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Example batch script for running example 3 on a SLURM cluster
+
+# set the number of nodesT
+#SBATCH --nodes=4
+
+# set the number of tasks (processes) per node.
+#SBATCH --ntasks-per-node=64
+
+# set max wallclock time for the entire fitting job (1 day)
+#SBATCH --time=1-00:00:00
+
+# set name of job
+#SBATCH --job-name=pybnf
+
+# Enable Anaconda Python 3.5
+# Your cluster might require something different here, or might not require anything. 
+module load anaconda/Anaconda3
+
+# Run PyBNF
+pybnf -c examples/example3/example3.conf -t SLURM
