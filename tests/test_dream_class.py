d7fccb0e7a82d9fa1baf652b74eff4a0e58747eb
Eshan Mitra <emitra@lanl.gov>
2018-05-23 09:55:53 -0600

    Converted __FREE__ to __FREE in Dream unit test

--- tests/test_dream_class.py
+++ tests/test_dream_class.py
@@ -19,7 +19,7 @@ class TestDream:
         cls.d1s = data.Data()
         cls.d1s.data = cls.d1s._read_file_lines(cls.data1s, '\s+')
 
-        cls.variables = ['v1__FREE__', 'v2__FREE__', 'v3__FREE__']
+        cls.variables = ['v1__FREE', 'v2__FREE', 'v3__FREE']
 
         cls.chi_sq = objective.ChiSquareObjective()
 
@@ -29,7 +29,7 @@ class TestDream:
         cls.config = config.Configuration({
             'population_size': 20, 'max_iterations': 20, 'step_size': 0.2, 'output_hist_every': 10, 'sample_every': 2,
             'burn_in': 3, 'credible_intervals': [68, 95], 'num_bins': 10, 'output_dir': 'noseoutput1/',
-            ('uniform_var', 'v1__FREE__'): [0., 0.5], ('loguniform_var', 'v2__FREE__'): [1., 10.], ('uniform_var', 'v3__FREE__'): [0, 10],
+            ('uniform_var', 'v1__FREE'): [0., 0.5], ('loguniform_var', 'v2__FREE'): [1., 10.], ('uniform_var', 'v3__FREE'): [0, 10],
             'models': {'bngl_files/parabola.bngl'}, 'exp_data': {'bngl_files/par1.exp'}, 'initialization': 'lh',
             'bngl_files/parabola.bngl': ['bngl_files/par1.exp'], 'fit_type': 'dream'})
 
@@ -42,11 +42,11 @@ class TestDream:
         dream = algorithms.DreamAlgorithm(self.config)
         start_psets = dream.start_run()
         assert len(dream.variables) == 3
-        assert sorted([v.name for v in dream.variables]) == ['v1__FREE__', 'v2__FREE__', 'v3__FREE__']
+        assert sorted([v.name for v in dream.variables]) == ['v1__FREE', 'v2__FREE', 'v3__FREE']
         assert len(start_psets) == 20
-        assert dream.prior['v1__FREE__'] == ('reg', 'b', 0., 0.5)
-        assert dream.prior['v2__FREE__'] == ('log', 'b', 0., 1.)
-        assert dream.prior['v3__FREE__'] == ('reg', 'b', 0, 10)
+        assert dream.prior['v1__FREE'] == ('reg', 'b', 0., 0.5)
+        assert dream.prior['v2__FREE'] == ('log', 'b', 0., 1.)
+        assert dream.prior['v3__FREE'] == ('reg', 'b', 0, 10)
 
     def test_update(self):
         dream = algorithms.DreamAlgorithm(self.config)
