888334ec04ae33563895233358bf7631f4dfffd5
Eshan Mitra <emitra@lanl.gov>
2018-08-31 14:41:43 -0600

    Update version number to 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Ryan Suderman, Eshan Mitra'
 author = 'Ryan Suderman, Eshan Mitra'
 
 # The short X.Y version
-version = '0.2.1'
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.1'
+release = 'v0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
