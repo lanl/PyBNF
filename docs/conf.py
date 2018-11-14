ebe8c088f9d610851ac8466620b5122d79fc11fb
Eshan Mitra <emitra@lanl.gov>
2018-11-14 09:18:56 -0700

    Update to version 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Ryan Suderman, Eshan Mitra'
 author = 'Ryan Suderman, Eshan Mitra'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.3'
+release = 'v0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- pybnf/pybnf.py
+++ pybnf/pybnf.py
@@ -21,7 +21,7 @@ import traceback
 import pickle
 
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 def main():
