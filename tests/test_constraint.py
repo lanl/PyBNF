1c59c832f33395a571c3fa247af404fd7d9eed0a
Eshan Mitra <emitra@lanl.gov>
2019-03-27 15:16:56 -0600

    Add support for using logit likelihood instead of static penalty

--- tests/test_constraint.py
+++ tests/test_constraint.py
@@ -1,6 +1,8 @@
 from .context import constraint, data
 import copy
 import numpy as np
+from nose.tools import raises
+from pyparsing import ParseBaseException
 
 
 class TestConstraint:
@@ -15,9 +17,16 @@ class TestConstraint:
         cls.line3 = 'A<16 between B=3.14, 702 weight 8 min 9'
         cls.line4 = 'A<16 always weight 6'
         cls.line5 = 'A<16 once weight 6'
+        cls.line6 = 'A<B at 6 confidence 0.98'
+        cls.line7 = 'A<16 between 5,B=5 confidence 0.95 tolerance 15'
+
+        cls.err1 = 'A<16 always weight 2 confidence 0.95'
+        cls.err2 = 'A<16 always confidence 0.98 tolerance 15 min 1'
+        cls.err3 = 'A<16 always tolerance 5'
 
         cls.f1 = 'bngl_files/p1_5.prop'
         cls.f2 = 'bngl_files/con_test.prop'
+        cls.f3 = 'bngl_files/con_test_likelihood.prop'
         cls.dat2 = 'bngl_files/con_test.gdat'
 
         cls.model = 'a.bngl'
@@ -59,6 +68,27 @@ class TestConstraint:
         p = self.cset.parse_constraint_line(self.line4)
         assert p.enforce[0] == 'always'
         assert p.weight_expr.weight == '6'
+
+    def test_grammar_likelihood(self):
+        p = self.cset.parse_constraint_line(self.line6)
+        assert not p.weight_expr
+        assert p.likelihood_expr.confidence == '0.98'
+        assert not p.likelihood_expr.tolerance
+        p = self.cset.parse_constraint_line(self.line7)
+        assert p.likelihood_expr.confidence == '0.95'
+        assert p.likelihood_expr.tolerance == '15'
+
+    @raises(ParseBaseException)
+    def test_grammar_invalid1(self):
+        p = self.cset.parse_constraint_line(self.err1)
+
+    @raises(ParseBaseException)
+    def test_grammar_invalid2(self):
+        p = self.cset.parse_constraint_line(self.err2)
+
+    @raises(ParseBaseException)
+    def test_grammar_invalid3(self):
+        p = self.cset.parse_constraint_line(self.err3)
 
     def test_load_file(self):
         cs = copy.deepcopy(self.cset)
@@ -125,3 +155,18 @@ class TestConstraint:
         np.testing.assert_almost_equal(cs.constraints[3].penalty(d_dict), 0.8)
 
         np.testing.assert_almost_equal(cs.total_penalty(d_dict), 170.6)
+
+    def test_likelihood_penalties(self):
+        d = data.Data()
+        d.load_data('bngl_files/con_test.gdat')
+        d_dict = {self.model: {self.suf: d}}
+
+        cs = copy.deepcopy(self.cset)
+        cs.load_constraint_file(self.f3)
+
+        assert cs.constraints[0].penalty(d_dict) == 0
+        np.testing.assert_almost_equal(cs.constraints[1].penalty(d_dict), -np.log(0.95))
+        np.testing.assert_almost_equal(cs.constraints[2].penalty(d_dict), -np.log(0.05))
+        np.testing.assert_almost_equal(cs.constraints[3].penalty(d_dict), -np.log(0.5))
+        np.testing.assert_almost_equal(cs.constraints[4].penalty(d_dict), -np.log(0.9/(1+np.exp(1)) + 0.05))
+        np.testing.assert_almost_equal(cs.constraints[5].penalty(d_dict), -np.log(0.9 / (1 + np.exp(-0.5)) + 0.05))
