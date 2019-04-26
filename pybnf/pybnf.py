33cf8adb5e79d08625de7e5414558c371be6f0a3
Eshan Mitra <emitra@lanl.gov>
2019-04-26 08:26:34 -0600

    Change mkdir() to makedirs() for recreating Simulations folder

--- pybnf/pybnf.py
+++ pybnf/pybnf.py
@@ -122,7 +122,7 @@ def main():
 
             logger.debug('Checking for Simulations directory')
             if not os.path.exists(alg.sim_dir):
-                os.mkdir(alg.sim_dir)
+                os.makedirs(alg.sim_dir)
 
             if alg.bootstrap_number is not None:
                 print0('Resuming a bootstrapping run')
